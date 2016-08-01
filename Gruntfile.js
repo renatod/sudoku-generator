e0f6c3fee87b24c67652cedd53f1db9178db947f
renatod <renato.dias@me.com.br>
2016-08-01 19:19:09 -0300

    adding bower

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,21 +4,56 @@ module.exports = function (grunt) {
     grunt.initConfig({
 
         // Task configuration.
+        bower: {
+          install: {
+              options: {
+                  targetDir: './vendor',
+                  cleanTargetDir: true,
+                  layout: 'byType'
+              }
+          }
+        },
+
         browserify: {
           dist: {
             files: {
-            'main.js': ['sudoku.js', 'src/*.js']
+            'dist/main.js': ['sudoku.js', 'src/*.js']
             }
           }
+        },
+
+        copy: {
+            site: {
+                src: ['vendor/**'],
+                dest: 'site/dist/'
+            }
+        },
+
+        jade: {
+            site: {
+                files: {
+                    "site/dist/index.html": ["site/index.jade"]
+                }
+            }
         }
 
     });
 
     // Load the plugin(s)
+    grunt.loadNpmTasks('grunt-bower-task');
     grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-jade');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
     // Default task(s).
     grunt.registerTask('default', [
         "browserify"
     ]);
+
+    grunt.registerTask('site', [
+        "copy:site",
+        "jade:site"
+    ]);
+
 };
